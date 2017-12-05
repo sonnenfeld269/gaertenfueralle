3802975649332384234fc5f91e0b0893403bb2ab
David Ifraimov <david.sonnenfeld269@gmail.com>
2017-12-05 15:45:40 +0100

    update frontend

--- js/scrolling.js
+++ js/scrolling.js
@@ -3,8 +3,8 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top - 40
+        }, 'slow');
         event.preventDefault();
     });
 });
